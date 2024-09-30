7e0bce284beabd90ad5e5c8e3652854aa4fd3d65
Georgy Zaets <georgezaets@gmail.com>
2024-09-29 18:51:48 -0700

    Updating git ignore and sorting GPU files into a dedicated folder that is not on repo for training

--- fridge-ai/datasets/test_gpu.py
+++
@@ -1,8 +0,0 @@
-import torch_directml
-
-# Create a DirectML device
-dml = torch_directml.device()
-
-# Create a tensor on the DirectML device
-x = torch.tensor([1.0, 2.0, 3.0, 4.0], device=dml)
-print(f'Tensor on DML device: {x}')
